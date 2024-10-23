f5533689d1d2e3d407d9666a2fc8586ee3521626
Leima Singha <leimasingha@Leimas-MacBook-Air.local>
2024-10-23 14:55:03 +0530

    My 3D Book Slider

--- src/components/UI.jsx
+++ src/components/UI.jsx
@@ -52,9 +52,9 @@ export const UI = () => {
       <main className=" pointer-events-none select-none z-10 fixed  inset-0  flex justify-between flex-col">
         <a
           className="pointer-events-auto mt-10 ml-10"
-          href="https://lessons.wawasensei.dev/courses/react-three-fiber"
+          // href="https://lessons.wawasensei.dev/courses/react-three-fiber"
         >
-          <img className="w-20" src="/images/wawasensei-white.png" />
+          <img className="w-20" src="/images/logo.png" />
         </a>
         <div className="w-full overflow-auto pointer-events-auto flex justify-center">
           <div className="overflow-auto flex items-center gap-4 max-w-full p-10">
@@ -89,54 +89,54 @@ export const UI = () => {
         <div className="relative">
           <div className="bg-white/0  animate-horizontal-scroll flex items-center gap-8 w-max px-8">
             <h1 className="shrink-0 text-white text-10xl font-black ">
-              Wawa Sensei
+              Express your Thoughts
             </h1>
             <h2 className="shrink-0 text-white text-8xl italic font-light">
-              React Three Fiber
+              Quotes
             </h2>
             <h2 className="shrink-0 text-white text-12xl font-bold">
-              Three.js
+              Poems
             </h2>
             <h2 className="shrink-0 text-transparent text-12xl font-bold italic outline-text">
-              Ultimate Guide
+            Best pictures
             </h2>
             <h2 className="shrink-0 text-white text-9xl font-medium">
-              Tutorials
+            Learning Three.js
             </h2>
             <h2 className="shrink-0 text-white text-9xl font-extralight italic">
-              Learn
+              *Learn*
             </h2>
             <h2 className="shrink-0 text-white text-13xl font-bold">
-              Practice
+              *Practice*
             </h2>
             <h2 className="shrink-0 text-transparent text-13xl font-bold outline-text italic">
-              Creative
+              *Create*
             </h2>
           </div>
           <div className="absolute top-0 left-0 bg-white/0 animate-horizontal-scroll-2 flex items-center gap-8 px-8 w-max">
             <h1 className="shrink-0 text-white text-10xl font-black ">
-              Wawa Sensei
+             Express your Thoughts
             </h1>
             <h2 className="shrink-0 text-white text-8xl italic font-light">
-              React Three Fiber
+              Quotes
             </h2>
             <h2 className="shrink-0 text-white text-12xl font-bold">
-              Three.js
+              Poems
             </h2>
             <h2 className="shrink-0 text-transparent text-12xl font-bold italic outline-text">
-              Ultimate Guide
+            Best pictures
             </h2>
             <h2 className="shrink-0 text-white text-9xl font-medium">
-              Tutorials
+            Learning Three.js
             </h2>
             <h2 className="shrink-0 text-white text-9xl font-extralight italic">
-              Learn
+              *Learn*
             </h2>
             <h2 className="shrink-0 text-white text-13xl font-bold">
-              Practice
+              *Practice*
             </h2>
             <h2 className="shrink-0 text-transparent text-13xl font-bold outline-text italic">
-              Creative
+              *Create*
             </h2>
           </div>
         </div>
